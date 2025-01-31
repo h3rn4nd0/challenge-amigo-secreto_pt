03d63f3f5850089a05f66287ead23c4093753aff
h3rn4nd0 <hernandomarques92@gmail.com>
2025-01-31 09:20:26 -0300

    implementando um sorteador de amigo secreto.

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function fAdicionarAmigo() {
+  let amigo = document.querySelector('input').value;
+  if (amigo == "") {
+    alert("Por favor, insira um nome.");
+  } else {
+    listaAmigos.push(amigo);
+  fLimparInput();
+  //let ami = listaAmigos[listaAmigos.length - 1];
+  
+  let novoItem = document.createElement('li');
+  novoItem.textContent = amigo;
+  
+  document.getElementById('listaAmigos').appendChild(novoItem);
+  }
+}
+function fLimparInput() {
+  let campo = document.querySelector('input');
+  campo.value = ('');
+}
+
+function fExibirNomes(tag, texto) {
+  campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+function fSortearAmigo() {
+  let indice = parseInt(Math.random()*listaAmigos.length);
+  let resultado = `Seu amigo secreto sorteado é: ${listaAmigos[indice]}`;
+  fExibirNomes('ul', resultado);
+}
